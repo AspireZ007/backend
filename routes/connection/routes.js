c4177807d196f1ffb8d8946b49386f44051aed79
Minnikeswar Rao <minnikes797@gmail.com>
2023-06-13 21:39:58 +0530

    modified the flow in connection routes

--- routes/connection/routes.js
+++ routes/connection/routes.js
@@ -20,7 +20,7 @@ const router = express.Router()
 router.use(checkJwt)
 
 /**
- * @param {Object} req - The HTTP request object (contains the follower and following UUIDs)
+ * @param {Object} req - The HTTP request object (contains the follower and followee UUIDs)
  * @param {Object} res - The HTTP response object.
  */
 router.post('/create', async (req, res) => {
@@ -31,103 +31,109 @@ router.post('/create', async (req, res) => {
 		return res.status(400).send(error.details[0].message) // send a clear error message if validation fails
 
 	const userId = req.userId // from the middleware checkJWT
-	const { followingId } = req.body
+	const { followeeId } = req.body
 
-	//chech if the follower user exists
+	if (!followeeId) {
+		return res.status(400).json(generateResponseMessage("error", "Missing required parameters"))
+	}
+
+	//check if the follower user exists
 	const followerExists = await isUserActive(userId)
-	const followeeExists = await isUserActive(followingId)
 
-	if (followeeExists === 1 && followerExists === 1) {
-		try {
-			const existingConnection = await Connection.find({ following: followingId, follower: userId })
-	
-			//check if the connection already exists
-			if (existingConnection.length > 0) {
-				return res.status(409).json(generateResponseMessage("error", "Connection already exists"))
-			}
-	
-			//completing all the validations and creating the connection
-			const connection = new Connection({ following: followingId, follower: userId })
-			await connection.save()
-			return res.status(200).json(generateResponseMessage("success", "Connection created successfully"))
+	if (followerExists !== 1) {
+		const outputString =  "The asker's account is " +
+			(followerExists == -1) ? "throwing a db error" :
+			(followerExists == -2) ? "not a valid id" :
+			(followerExists == 0) ? "not found with this id" :
+			(followerExists == 2) ? "is only temporarily registered" :
+			(followerExists == 3) ? "is banned" : `!! returning ${followerExists}`
+		return res.status(403).json(generateResponseMessage("error", outputString))
+	} 
+
+	const followeeExists = await isUserActive(followeeId)
+
+	if (followeeExists !== 1){
+		const outputString =  "The target to follow's account is " +
+			(followeeExists == -1) ? "throwing a db error" :
+			(followeeExists == -2) ? "not a valid id" :
+			(followeeExists == 0) ? "not found with this id" :
+			(followeeExists == 2) ? "is only temporarily registered" :
+			(followeeExists == 3) ? "is banned" : `!! returning ${followeeExists}`
+		return res.status(403).json(generateResponseMessage("error", outputString))
+	}
+
+	try {
+		const existingConnection = await Connection.find({ followee: followeeId, follower: userId })
+
+		//check if the connection already exists
+		if (existingConnection.length > 0) {
+			return res.status(409).json(generateResponseMessage("error", "Connection already exists"))
 		}
-		catch (error) {
-			logger.error(error)
-			return res.status(500).json(generateResponseMessage("error", error))
-		}
-	} else {
-		if (followerExists !== 1) {
-			const outputString =  "The asker's account is " +
-				(followerExists == -1) ? "throwing a db error" :
-				(followerExists == -2) ? "not a valid id" :
-				(followerExists == 0) ? "not found with this id" :
-				(followerExists == 2) ? "is only temporarily registered" :
-				(followerExists == 3) ? "is banned" : `!! returning ${followerExists}`
-			return res.status(403).json(generateResponseMessage("error", "The asker's account is "))
-		} else {
-			const outputString =  "The target to follow's account is " +
-				(followerExists == -1) ? "throwing a db error" :
-				(followerExists == -2) ? "not a valid id" :
-				(followerExists == 0) ? "not found with this id" :
-				(followerExists == 2) ? "is only temporarily registered" :
-				(followerExists == 3) ? "is banned" : `!! returning ${followerExists}`
-			return res.status(403).json(generateResponseMessage("error", "The asker's account is "))
-		}
+
+		//completing all the validations and creating the connection
+		const connection = new Connection({ followee: followeeId, follower: userId })
+		await connection.save()
+		return res.status(200).json(generateResponseMessage("success", "Connection created successfully"))
+	}
+	catch (error) {
+		logger.error(error)
+		return res.status(500).json(generateResponseMessage("error", error))
 	}
 })
 
 /**
- * @param {Object} req - The HTTP request object (contains the follower and following UUIDs)
+ * @param {Object} req - The HTTP request object (contains the follower and followee UUIDs)
  * @param {Object} res - The HTTP response object.
  */
 router.post('/remove', async (req, res) => {
 
-	// unpacking the follower and following UUIDs from the request body
-	const { followerId, followingId } = req.body
+	const { error } = connectValidator.validate(req.body) // validate the request body
+	if (error)
+		return res.status(400).send(error.details[0].message) // send a clear error message if validation fails
+
+	const userId = req.userId // from the middleware checkJWT
+	const { followeeId } = req.body
 
 	//check for the case when null is passed as a parameter
-	if (!followerId || !followingId) {
+	if (!followeeId) {
 		return res.status(400).json(generateResponseMessage("error", "Missing required parameters"))
 	}
 
-	//chech if the follower user exists
-	const followerExists = await isUserActive(followerId)
-	if (followerExists === 0) {
-		return res.status(404).json(generateResponseMessage("error", "Follower user does not exist"))
-	}
-	else if (followerExists === -1) {
-		return res.status(500).json(generateResponseMessage("error", "Internal Server Error"))
-	}
-	else if (followerExists === -2) {
-		return res.status(500).json(generateResponseMessage("error", "Database constraints violated"))
-	}
+	const followerExists = await isUserActive(userId)
 
-	//check if the following user exists
-	const followingExists = await isUserActive(followingId)
-	if (followingExists === 0) {
-		return res.status(404).json(generateResponseMessage("error", "Following user does not exist"))
-	}
-	else if (followingExists === -1) {
-		return res.status(500).json(generateResponseMessage("error", "Internal Server Error"))
-	}
-	else if (followerExists === -2) {
-		return res.status(500).json(generateResponseMessage("error", "Database constraints violated"))
+	if (followerExists !== 1) {
+		const outputString =  "The asker's account is " +
+			(followerExists == -1) ? "throwing a db error" :
+			(followerExists == -2) ? "not a valid id" :
+			(followerExists == 0) ? "not found with this id" :
+			(followerExists == 2) ? "is only temporarily registered" :
+			(followerExists == 3) ? "is banned" : `!! returning ${followerExists}`
+		return res.status(403).json(generateResponseMessage("error", outputString))
+	} 
+
+	const followeeExists = await isUserActive(followeeId)
+
+	if (followeeExists !== 1){
+		const outputString =  "The target to follow's account is " +
+			(followeeExists == -1) ? "throwing a db error" :
+			(followeeExists == -2) ? "not a valid id" :
+			(followeeExists == 0) ? "not found with this id" :
+			(followeeExists == 2) ? "is only temporarily registered" :
+			(followeeExists == 3) ? "is banned" : `!! returning ${followeeExists}`
+		return res.status(403).json(generateResponseMessage("error", outputString))
 	}
 
 	try {
 
 		//check if the connection exists or not
-		const existingConnection = await Connection.find({ following: followingId, follower: followerId })
+		const existingConnection = await Connection.find({ followee: followeeId, follower: userId })
 		if (existingConnection.length === 0) {
 			return res.status(404).json(generateResponseMessage("error", "Connection does not exist"))
 		}
-		else if (existingConnection.length > 1) {
-			return res.status(500).json(generateResponseMessage("error", "Database constraints violated"))
-		}
 
 		//completing all the validations and deleting the connection
-		const deletedConnection = await Connection.deleteOne({ following: followingId, follower: followerId })
-		return res.status(200).json(generateResponseMessage("success", "Connection deleted successfully", deletedConnection))
+		const deletedConnection = await Connection.deleteOne({ followee: followeeId, follower: userId })
+		return res.status(200).json(generateResponseMessage("success", "Connection deleted successfully"))
 	}
 	catch (error) {
 		logger.error(error)
